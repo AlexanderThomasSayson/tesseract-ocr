6c9db94695d9c59fb42da4c9ad54751a630e8f48
Alex <alex.srbootcamp@gmail.com>
2025-03-25 16:47:08 +0800

    feat: add image loading failure exception.

---
+++ src/main/java/com/srllc/tesseract_ocr/exception/FailedToLoadImageException.java
@@ -0,0 +1,7 @@
+package com.srllc.tesseract_ocr.exception;
+
+public class FailedToLoadImageException extends RuntimeException {
+    public FailedToLoadImageException(String message) {
+        super(message);
+    }
+}
