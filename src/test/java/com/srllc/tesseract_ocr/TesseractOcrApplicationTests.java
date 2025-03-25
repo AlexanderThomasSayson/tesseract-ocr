51b476fd1b8ada545cec7d7b6695e611f528b34c
Alex <alex.srbootcamp@gmail.com>
2025-03-25 15:31:17 +0800

    initial commit

---
+++ src/test/java/com/srllc/tesseract_ocr/TesseractOcrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srllc.tesseract_ocr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesseractOcrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
