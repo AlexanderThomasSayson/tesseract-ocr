b15bf37997677a4208a75a282ca01ebd44ffcf46
Alex <alex.srbootcamp@gmail.com>
2025-04-29 15:40:17 +0800

    feat: add aws textract configurations.

---
+++ src/main/java/com/srllc/tesseract_ocr/config/AwsTextractConfig.java
@@ -0,0 +1,32 @@
+package com.srllc.tesseract_ocr.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.textract.TextractClient;
+
+@Configuration
+public class AwsTextractConfig {
+
+    @Value("${aws.accessKeyId}")
+    private String accessKeyId;
+
+    @Value("${aws.secretAccessKey}")
+    private String secretAccessKey;
+
+    @Value("${aws.region}")
+    private String region;
+
+    @Bean
+    public TextractClient textractClient() {
+        return TextractClient.builder()
+                .region(Region.of(region)) // <- this is the missing part
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKeyId, secretAccessKey)
+                ))
+                .build();
+    }
+}
